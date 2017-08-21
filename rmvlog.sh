bc14bab8f465d3b2f3d065c3f9425c13c5fbb2df
kvaps <kvapss@gmail.com>
2017-08-21 15:32:32 +0200

    add: timeout

--- rmvlog.sh
+++ rmvlog.sh
@@ -4,6 +4,7 @@ LOG_DIR="$(dirname "$1")"
 LOG_NAME="$(basename "$1")"
 SERVER="$(echo $2 | grep -oP "^.*(?=:)")"
 NEW_LOG_DIR="$(echo $2 | grep -oP "[^:]*$")"
+TIMEOUT=${3:-5}
 
 SERVER="${SERVER:-localhost}"
 NEW_LOG_DIR=${NEW_LOG_DIR%/}
@@ -15,14 +16,20 @@ fi
 
 cd "$LOG_DIR"
 
-while read LOG_FILE; do
-    if [ -f "$LOG_FILE.sync" ]; then
-        mv "$LOG_FILE" "$LOG_FILE.sync"
-    else
-        cat "$LOG_FILE" >> "$LOG_FILE.sync" && rm "$LOG_FILE"
-    fi
-done < <(eval find . -maxdepth 1 -type f -name \'$LOG_NAME\' ! -empty)
+while true; do
 
-while read SYNC_FILE; do
-    rm "$SYNC_FILE"
-done < <(eval find . -maxdepth 1 -type f -name \'$LOG_NAME.sync\' ! -empty -printf \'%P\\n\' | tar -czT - -f - | ssh $SERVER "tar -xzf - --to-command='sh -c \"cat >> \\\"${NEW_LOG_DIR}/\$(basename \$TAR_FILENAME .sync)\\\" && echo \$TAR_FILENAME\"'")
+    while read LOG_FILE; do
+        if [ -f "$LOG_FILE.sync" ]; then
+            mv "$LOG_FILE" "$LOG_FILE.sync"
+        else
+            cat "$LOG_FILE" >> "$LOG_FILE.sync" && rm "$LOG_FILE"
+        fi
+    done < <(eval find . -maxdepth 1 -type f -name \'$LOG_NAME\' ! -empty)
+
+    while read SYNC_FILE; do
+        rm "$SYNC_FILE"
+    done < <(eval find . -maxdepth 1 -type f -name \'$LOG_NAME.sync\' ! -empty -printf \'%P\\n\' | tar -czT - -f - | ssh $SERVER "tar -xzf - --to-command='sh -c \"cat >> \\\"${NEW_LOG_DIR}/\$(basename \$TAR_FILENAME .sync)\\\" && echo \$TAR_FILENAME\"'")
+
+    sleep $TIMEOUT
+
+done
